24e107e82ac21899b8a0389828d04f525b4ddac0
nataliazalucka <natalia.zalucka4@gmail.com>
2026-04-02 15:07:32 +0200

    Add GET test- status code 200

--- src/test/java/tests/GetUsersTest.java
+++ src/test/java/tests/GetUsersTest.java
@@ -11,11 +11,12 @@ import static io.restassured.RestAssured.given;
 
 public class GetUsersTest {
 
-
         @BeforeClass
         public void setUp() {
             RestAssured.baseURI = "https://reqres.in/api";
-            RestAssured.requestSpecification=given().header("x-api-key",
+            RestAssured.requestSpecification=
+                    given()
+                    .header("x-api-key",
                     "reqres_a9db5cb7d8c849029d3ba03cf8e6ddb5");
         }
 
@@ -26,6 +27,18 @@ public class GetUsersTest {
                     .get("/users/2")
                     .then()
                     .statusCode(200);
+        }
+        @Test(description = "Get user — verify email")
+    public void getUserEmail(){
+         Response response=
+                    given()
+                    .when()
+                    .get("/users/2")
+                    .then()
+                    .statusCode(200)
+                    .extract().response();
+            String email=response.jsonPath().getString("data.email");
+            assertEquals(email,"janet.weaver@reqres.in","email doesnt exist");
         }
 
 }
