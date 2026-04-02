4d8901039c94757a83f9b63eece305e8e7778aec
nataliazalucka <natalia.zalucka4@gmail.com>
2026-04-02 12:53:18 +0200

    Add GET test- status code 200

---
+++ src/test/java/tests/GetUsersTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+import static org.testng.Assert.assertEquals;
+
+import static io.restassured.RestAssured.given;
+
+public class GetUsersTest {
+
+
+        @BeforeClass
+        public void setUp() {
+            RestAssured.baseURI = "https://reqres.in/api";
+            RestAssured.requestSpecification=given().header("x-api-key",
+                    "reqres_a9db5cb7d8c849029d3ba03cf8e6ddb5");
+        }
+
+        @Test(description = "Get user — verify status code is 200")
+        public void getUserStatusCode() {
+            given()
+                    .when()
+                    .get("/users/2")
+                    .then()
+                    .statusCode(200);
+        }
+
+}
