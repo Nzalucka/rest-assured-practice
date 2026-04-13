d96785869bbe9e7b9704e867bf9fe759d4982e41
nataliazalucka <natalia.zalucka4@gmail.com>
2026-04-13 11:22:25 +0200

    Add PATCH user test — verify job updated

---
+++ src/test/java/tests/PatchUserTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+public class PatchUserTest extends BaseTest{
+    @Test(description = "Patch user — verify status code 200")
+    public void patchUserJob(){
+        Response response=
+                given().
+                        contentType("application/json")
+                        .body("{\"job\": \"Analyst\"}").when().patch("/users/2")
+                        .then().statusCode(200).extract().response();
+        String job=response.jsonPath().getString("job");
+        System.out.println(job);
+        assertEquals(job,"Analyst","job should be analyst");
+    }
+
+
+}
