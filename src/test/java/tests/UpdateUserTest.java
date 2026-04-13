cf74b86dd40bbe7951e1f9bd737033986e0c7b9f
nataliazalucka <natalia.zalucka4@gmail.com>
2026-04-13 11:06:59 +0200

    Add PUT tests — verify updatedAt is not null

--- src/test/java/tests/UpdateUserTest.java
+++ src/test/java/tests/UpdateUserTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class UpdateUserTest extends BaseTest{
     @Test(description = "Update user — verify status code 200")
@@ -27,6 +28,30 @@ public class UpdateUserTest extends BaseTest{
         String name=response.jsonPath().getString("name");
         System.out.println(name);
         assertEquals(name,"Natalia-Updated","name should be Natalia-Updated");
+    }
+    @Test(description = "Update user — verify job in response")
+    public void updateUserVerifyjob(){
+        Response response=
+                given().contentType("application/json")
+                        .body("{\"name\": \"Natalia-Updated\", \"job\": \"QA Senior\"}")
+                        .when().put("/users/2")
+                        .then().statusCode(200).extract().response();
+        String job=response.jsonPath().getString("job");
+        System.out.println(job);
+        assertEquals(job,"QA Senior","job should be QA Senior");
+    }
+    @Test(description = "Update user — verify updatedAt is not null")
+    public void updateUserVerifyUpdatedAt(){
+        Response response=
+                given()
+                        .contentType("application/json")
+                        .body("{\"name\": \"Natalia-Updated\", \"job\": \"QA Senior\"}")
+                        .when().put("/users/2")
+                        .then().statusCode(200).extract().response();
+        String updatedAt = response.jsonPath().getString("updatedAt");
+        System.out.println(updatedAt);
+        assertNotNull(updatedAt, "updatedAt should not be null");
+
     }
 
 
