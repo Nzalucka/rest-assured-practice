8c200a1864ae27e39f3b859d8d19b2b29c8f11c0
nataliazalucka <natalia.zalucka4@gmail.com>
2026-04-29 17:11:24 +0200

    Add JSON Schema validation for users list

---
+++ src/test/java/tests/JsonSchemaTest.java
@@ -0,0 +1,38 @@
+package tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class JsonSchemaTest extends BaseTest {
+
+    @Story("JSON Schema validation")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify GET /users/2 response matches JSON schema")
+    @Test(description = "Get user — verify JSON schema")
+    public void getUserMatchesSchema(){
+        given()
+                .when().get("/users/2")
+                .then()
+                .spec(responseSpec)
+                .body(matchesJsonSchemaInClasspath("schemas/user-schema.json"));
+
+    }
+
+    @Story("JSON Schema validation")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify GET /users response matches JSON schema")
+    @Test(description = "Get user — verify JSON schema")
+    public void getUsersListMatchesSchema(){
+        given().when().get("/users")
+                .then().spec(responseSpec)
+                .body(matchesJsonSchemaInClasspath("schemas/users-list-schema.json"));
+
+    }
+
+}
