6e7373128dcd7b5e3420a56547aeb2b6f331e93f
nataliazalucka <natalia.zalucka4@gmail.com>
2026-05-14 14:28:32 +0200

    Add Spotify searchArtist test with assertions

--- src/test/java/spotify/SearchTest.java
+++ src/test/java/spotify/SearchTest.java
@@ -4,9 +4,13 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class SearchTest extends SpotifyBaseTest{
     @Story("Spotify Search")
@@ -14,13 +18,30 @@ public class SearchTest extends SpotifyBaseTest{
     @Description("Search for Coldplay artist")
     @Test(description = "Search artist — verify status 200")
     public void searchArtist() {
-        given()
+
                 //.baseUri(SpotifyTestData.BASE_API_URI)
+           Response response=
+                   given()
                 .spec(requestSpecification)
              //   .header("Authorization","Bearer "+token)
                 .queryParam("q","Coldplay")
                 .queryParam("type","artist")
                 .when().get("/search")
-                .then().spec(responseSpecification).statusCode(200);
+                .then().spec(responseSpecification).statusCode(200)
+                .body("artists.total", greaterThan(0))
+                .body("artists.items.find " +
+                        "{it.id=='4gzpq5DPGxSnKTe4SA8HAU' " +
+                        "}.name",equalTo("Coldplay"))
+                .body("artists.items.find { " +
+                        "it.id == '4gzpq5DPGxSnKTe4SA8HAU' " +
+                        "}.id", notNullValue())
+                .body("artists.items.find {it.id=='4gzpq5DPGxSnKTe4SA8HAU'}.type",equalTo("artist"))
+                           .extract().response();
+
+        Map<String,Object>coldplay=response.jsonPath().getMap("artists.items.find { it.id == '4gzpq5DPGxSnKTe4SA8HAU' }");
+        System.out.println("coldplay: "+coldplay);
+
+
+
     }
 }
